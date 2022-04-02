48447586b15560164c2932f1e622f03ae71b5368
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-04-02 15:13:59 +0300

    add Car with its names, type of car, type of engine. Create Enum with type of engine: GAS, DIESEL, BENZINE and ELECTRIC

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car carFirst = new Car("Tesla-Andrew", "Tesla", Engine.ELECTRIC);
+        System.out.println(carFirst.calculateThePollutionCoefficient());
+        Car carSecond = new Car("Tesla-Andrew", "Tesla", Engine.GAS);
+        System.out.println(carSecond.calculateThePollutionCoefficient());
+        Car carThree = new Car("Tesla-Andrew", "Tesla", Engine.BENZINE);
+        System.out.println(carThree.calculateThePollutionCoefficient());
+        Car carFour = new Car("Tesla-Andrew", "Tesla", Engine.DIESEL);
+        System.out.println(carFour.calculateThePollutionCoefficient());
+
+        Engine[] engines = Engine.values();
+        for(Engine value: engines){
+            System.out.println(value);
+        }
     }
 }
